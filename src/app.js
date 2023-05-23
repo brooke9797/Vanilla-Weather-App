473eee533d8f23b8cbc781ab25d6d8689e846a9c
brooke9797 <brooke97@comcast.net>
2023-05-23 18:39:37 -0400

    Fixed the javascript time update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 function formatDate(timestamp) {
     let date = new Date(timestamp);
     let hours = date.getHours();
-    if (hours < 10){
-        hours = '0${hours}';
+    if (hours < 10) {
+      hours = `0${hours}`;
     }
     let minutes = date.getMinutes();
-    if (minutes < 10){
-        minutes = '0${minutes}';}
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
     let days = [
-        "Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
     let day = days[date.getDay()];
-    return '${day} ${hours}:${minutes}';
-}
+    return `${day} ${hours}:${minutes}`;
+  }
 
 function displayTemperature(response) {
     console.log(response.data);
